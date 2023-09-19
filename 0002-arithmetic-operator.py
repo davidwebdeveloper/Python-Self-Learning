b4a44a2d10f7cf3389bf3d8ed4d55f30c3425568
davidwebdeveloper <mdd11024@gmail.com>
2023-09-19 17:58:26 +0530

    practice arithmetic operator

---
+++ 0002-arithmetic-operator.py
@@ -0,0 +1,10 @@
+# Arithmetic operator
+a = -7
+b = 2
+print('a + b = ', a+b)
+print('a - b = ', a-b)
+print('a * b = ', a*b)
+print('a / b = ', a/b)
+print('a % b = ', a % b)
+print('a // b = ', a//b)
+print('a ** b = ', a**b)
